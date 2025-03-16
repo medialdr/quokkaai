b3cd68cf06cc28f887228acee04af9fc721fd545
medialdr <medialdr@gmail.com>
2025-03-16 23:14:38 +0900

    Update Vite config for better build optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', '@mui/material', '@emotion/react', '@emotion/styled'],
+          charts: ['chart.js', 'react-chartjs-2']
+        }
+      }
+    }
+  }
 })
